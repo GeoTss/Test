93e92c3511314469d3c4e44bcf877893198eaa04
GeoTss <giorgosuncharted@gmail.com>
2024-11-19 04:18:27 +0200

    Added mul func

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int add(int a, int b){ return a+b; }
-
 int sub(int a, int b){ return a-b; }
+int mul(int a, int b){return a*b; }
 
 int main(){
     std::cout << "Hello world!!!\n";
